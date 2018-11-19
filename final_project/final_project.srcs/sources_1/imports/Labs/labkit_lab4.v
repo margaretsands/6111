7a1e9fdf427fba71ba15e6e83d9692b9dc56f264
Margaret E Sands <msands@mit.edu>
2018-11-19 18:24:12 -0500

    pls fix the question mark

--- final_project/final_project.srcs/sources_1/imports/Labs/labkit_lab4.v
+++ final_project/final_project.srcs/sources_1/imports/Labs/labkit_lab4.v
@@ -281,16 +281,25 @@ module CollisionDetection(
     input [9:0] wall_position,
     //input [9:0] wallDistance,
     //input [?:0] character_speed,
-    //input [?:0] wall_speed
-    output collision);
+    //input [?:0] xCollision
+    output reg collision);
     
+    //current height for character is the top of their box
     parameter characterSize = 64;
-    parameter wallHeight = 10;
     
+    //wall height is the height it comes down from the top of the screen
+//    parameter wallHeight = 10;
+    
+    //wall distance is the amount of space between the bottom of the top wall and the top of the bottom wall
     parameter [9:0] wallDistance = 100;
     
     //actually need to take in speed too rip
-    always @(character_height,wall_position) begin 
+    always @(*) begin
+        if ((character_height <= wall_position) || ((character_height+characterSize) >= (wall_position + wallDistance))) begin
+            collision <= 1;
+        end
+        else 
+            collision <= 0;
     end
 endmodule
 
